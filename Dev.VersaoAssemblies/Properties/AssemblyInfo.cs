0bd6010ca5257f1b052b3e28adecf0afc73756c4
Washington Reis <washingtonreis@autocom-mg.com.br>
2020-08-19 20:14:40 -0300

    - Alterado a versão para manter a compatibilidade com o pacote Nuget atual do eSistemLoja.

--- Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
+++ Dev.VersaoAssemblies/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.770.1")]
-[assembly: AssemblyFileVersion("0.0.770.1")]
+[assembly: AssemblyVersion("1.0.770.1")]
+[assembly: AssemblyFileVersion("1.0.770.1")]
